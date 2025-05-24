30b29d4349c374a2196465e21886d6ca239f8d99
Carlos Marques <carrlosmr@gmail.com>
2025-05-24 18:14:43 -0300

    feat: add middleware to validate request body with zod

---
+++ src/infra/middlewares/validation-body-middleware.ts
@@ -0,0 +1,54 @@
+import { ZodSchema, z } from "zod";
+import { APIGatewayProxyEventWithValidatedBody } from "../types/api-gateway";
+import { APIGatewayProxyEvent, APIGatewayProxyResult, Context, Handler } from "aws-lambda";
+import { HttpStatus } from "src/app/utils/http-status";
+
+type ValidatedHandler<S extends ZodSchema> = (
+    event: APIGatewayProxyEventWithValidatedBody<z.infer<S>>,
+    context: Context
+) => Promise<APIGatewayProxyResult>;
+
+export function validationBodyMiddleware<S extends ZodSchema>(
+    schema: S,
+    handlerFunction: ValidatedHandler<S>
+): Handler<APIGatewayProxyEvent, APIGatewayProxyResult>{
+
+    return async (event, context): Promise<APIGatewayProxyResult> => {
+        if(!event.body){
+            return {
+                statusCode: HttpStatus.BAD_REQUEST,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    message: "Body is required"
+                })
+            }
+        }
+
+        let requestBody;
+        try{
+            requestBody = JSON.parse(event.body)
+        }catch(error){
+            return {
+                statusCode: HttpStatus.BAD_REQUEST,
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    message: "Invalid request body"
+                })
+            }
+        }
+
+        const validationResult = schema.safeParse(requestBody)
+        if(!validationResult.success){
+            throw validationResult.error
+        }
+
+        const eventWithValidatedBody = event as APIGatewayProxyEventWithValidatedBody<z.infer<S>>
+        eventWithValidatedBody.validatedBody = validationResult.data
+
+        return handlerFunction(eventWithValidatedBody, context)
+    }
+}
