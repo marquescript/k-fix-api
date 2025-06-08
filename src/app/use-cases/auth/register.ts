5fd1e7f09810e0e1253141b5ad16ae40ae75592f
Carlos Marques <carrlosmr@gmail.com>
2025-06-08 00:43:51 -0300

    feat: creating register flow

---
+++ src/app/use-cases/auth/register.ts
@@ -0,0 +1,69 @@
+import { ResourceAlreadyExistsException } from "src/app/exceptions/resource-already-exists";
+import { Role } from "src/domain/@types/role";
+import { User } from "src/domain/@types/user";
+import { AuthenticationProvider } from "src/domain/providers/authentication-provider";
+import { EncryptionProvider } from "src/domain/providers/encryption-provider";
+import { LoggerProvider } from "src/domain/providers/logger-provider";
+import { UserRepository } from "src/domain/repository/user-repository";
+
+interface RegisterData {
+    name: string;
+    email: string;
+    password: string;
+}
+
+interface RegisterResponse {
+    token: string;
+    refreshToken: string;
+}
+
+export class RegisterUseCase {
+
+    constructor(
+        private readonly userRepository: UserRepository,
+        private readonly encryptionProvider: EncryptionProvider,
+        private readonly authenticationProvider: AuthenticationProvider,
+        private readonly loggerProvider: LoggerProvider
+    ){}
+
+    async execute(
+        { name, email, password }: RegisterData
+    ): Promise<RegisterResponse> {
+        this.loggerProvider.debug("RegisterUseCase.execute", { name, email, password })
+
+        const emailExists = await this.userRepository.findByEmail(email)
+
+        if(emailExists) throw new ResourceAlreadyExistsException("Email already in use")
+
+        const hashedPassword = await this.encryptionProvider.hash(password)
+
+        const dateNow = new Date()
+
+        const user: User = {
+            name,
+            email,
+            password: hashedPassword,
+            role: Role.USER,
+            createdAt: dateNow,
+            updatedAt: dateNow
+        }
+        
+        const userCreated = await this.userRepository.create(user)
+
+        const token = this.authenticationProvider.generateAccessToken(userCreated)
+        const refreshToken = this.authenticationProvider.generateRefreshToken(userCreated)
+
+        this.loggerProvider.info("User registered successfully", 
+            { 
+                user: {  
+                    id: userCreated.id,
+                    name: userCreated.name,
+                    email: userCreated.email,
+                } 
+            }
+        )
+
+        return { token, refreshToken }
+    }
+
+}
---
+++ src/infra/handlers/auth/register.ts
@@ -0,0 +1,66 @@
+import { z } from "zod";
+import { validationBodyMiddleware } from "../../middlewares/validation-body-middleware";
+import { APIGatewayProxyResult, Context } from "aws-lambda";
+import { APIGatewayProxyEventWithValidatedBody } from "src/infra/types/api-gateway";
+import { HttpStatus } from "src/app/utils/http-status";
+import { globalErrorHandlerMiddleware } from "src/infra/middlewares/global-error-handler-middleware";
+import { MongoDBUserRepository } from "src/infra/database/mongodb/mongodb-user-repository";
+import { BcryptAdapter } from "src/infra/adapters/bcryptjs-adapter";
+import { JsonWebTokenAdapter } from "src/infra/adapters/jsonwebtoken-adapter";
+import { RegisterUseCase } from "src/app/use-cases/auth/register";
+import connectToMongoDB from "src/infra/config/mongodb";
+import { winstonAdapter } from "src/infra/adapters/winston-adapter";
+
+const registerSchema = z.object({
+    name: z.string(),
+    email: z.string().email(),
+    password: z.string().min(6)
+})
+
+type RegisterInput = z.infer<typeof registerSchema>
+
+const dbConnectionPromise = connectToMongoDB()
+
+async function register(
+    event: APIGatewayProxyEventWithValidatedBody<RegisterInput>,
+    _: Context
+): Promise<APIGatewayProxyResult>{
+    winstonAdapter.info("SignInHandler.signIn", {
+        method: event.httpMethod,
+        url: event.requestContext.domainName + event.requestContext.path,
+        body: { 
+            name: event.validatedBody.name,
+            email: event.validatedBody.email
+        }
+    })
+
+    await dbConnectionPromise
+
+    const { name, email, password } = event.validatedBody
+
+    const userRepository = new MongoDBUserRepository()
+    const encryptionProvider = new BcryptAdapter()
+    const authenticationProvider = new JsonWebTokenAdapter()
+    const registerUseCase = new RegisterUseCase(
+        userRepository, 
+        encryptionProvider, 
+        authenticationProvider, 
+        winstonAdapter
+    )
+
+    const { token, refreshToken } = await registerUseCase.execute({name, email, password})
+
+    return {
+        statusCode: HttpStatus.CREATED,
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            data: { token, refreshToken }
+        })
+    }
+}
+
+export const handler = globalErrorHandlerMiddleware(
+    validationBodyMiddleware(registerSchema, register)
+)
